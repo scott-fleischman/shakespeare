1ffbf7740ca29db3f2524952e2937d25190aea5e
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2018-06-15 13:39:55 -0700

    Add some basic character analysis/extraction.

--- source/Hamlet.hs
+++ source/Hamlet.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import qualified Data.Char as Char
+-- import qualified Data.Char as Char
 import qualified Data.Set as Set
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text.IO
@@ -12,9 +12,10 @@ main = do
   hamletText <- Text.IO.readFile "hamlet.txt"
   let
     hamletLines = Text.lines hamletText
-    isNameLetterSpace x = Char.isUpper x || Char.isSpace x
+    hamletPastIntro = dropWhile (/= "SCENE. Elsinore.") hamletLines
+    isNameLetterSpace _x = True --Char.isUpper x || Char.isSpace x
     isNamePrefix x
       = Text.takeEnd 1 x == "."
       && (Text.all isNameLetterSpace . Text.dropEnd 1) x
-    hamletNames = (Set.fromList . filter isNamePrefix) hamletLines
+    hamletNames = (Set.fromList . filter isNamePrefix) hamletPastIntro
   mapM_ Text.IO.putStrLn hamletNames
