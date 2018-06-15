62c4105dcf8746e0ad8f3016f39c38428be76c4f
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2018-06-15 12:56:05 -0700

    Print characters.

--- source/Hamlet.hs
+++ source/Hamlet.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Data.Char as Char
+import qualified Data.Set as Set
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text.IO
 
@@ -8,4 +12,9 @@ main = do
   hamletText <- Text.IO.readFile "hamlet.txt"
   let
     hamletLines = Text.lines hamletText
-  print $ length hamletLines
+    isNameLetterSpace x = Char.isUpper x || Char.isSpace x
+    isNamePrefix x
+      = Text.takeEnd 1 x == "."
+      && (Text.all isNameLetterSpace . Text.dropEnd 1) x
+    hamletNames = (Set.fromList . filter isNamePrefix) hamletLines
+  mapM_ Text.IO.putStrLn hamletNames
