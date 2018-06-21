f4e059a1765bedd1c8ec45fccfcc44c96fddf9af
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2018-06-21 05:17:12 -0700

    Add line numbers.

--- source/Hamlet.hs
+++ source/Hamlet.hs
@@ -2,20 +2,56 @@
 
 module Main where
 
--- import qualified Data.Char as Char
-import qualified Data.Set as Set
+import           Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text.IO
+import qualified Data.Text.Lazy.IO as Text.Lazy.IO
+import           Formatting ((%))
+import qualified Formatting
+import           Prelude hiding (lines)
 
 main :: IO ()
 main = do
   hamletText <- Text.IO.readFile "hamlet.txt"
+  let lines = getTextLines hamletText
+  trails <- eitherToError . getTrails $ lines
   let
-    hamletLines = Text.lines hamletText
-    hamletPastIntro = dropWhile (/= "SCENE. Elsinore.") hamletLines
-    isNameLetterSpace _x = True --Char.isUpper x || Char.isSpace x
-    isNamePrefix x
-      = Text.takeEnd 1 x == "."
-      && (Text.all isNameLetterSpace . Text.dropEnd 1) x
-    hamletNames = (Set.fromList . filter isNamePrefix) hamletPastIntro
-  mapM_ Text.IO.putStrLn hamletNames
+    trailsWithTails = filter ((> 1) . trailBlankLines) trails
+    displayLine (Line (LineNumber n) t) = Formatting.format (Formatting.right 5 ' ' % Formatting.stext) n t
+    displayTrail (Trail l c) = Formatting.format (Formatting.right 5 ' ' % Formatting.text) c (displayLine l)
+  mapM_ (Text.Lazy.IO.putStrLn . displayTrail) trailsWithTails
+
+eitherToError :: Show a => Either a b -> IO b
+eitherToError (Left err) = fail . show $ err
+eitherToError (Right x) = return x
+
+data Line = Line
+  { lineNumber :: LineNumber
+  , lineText :: Text
+  } deriving Show
+newtype LineNumber = LineNumber Int deriving Show
+
+getTextLines :: Text -> [Line]
+getTextLines = zipWith Line lineNumbers . Text.lines
+  where
+  lineNumbers = fmap LineNumber [1..]
+
+data Trail = Trail
+  { trailLine :: Line
+  , trailBlankLines :: Int
+  } deriving Show
+
+data InitialBlankLine = InitialBlankLine deriving Show
+
+isBlankLine :: Line -> Bool
+isBlankLine = Text.null . lineText
+
+getTrails :: [Line] -> Either InitialBlankLine [Trail]
+getTrails [] = Right []
+getTrails (x : _) | isBlankLine x = Left InitialBlankLine
+getTrails (x : xs) = Right $ getTrailsRec x 0 xs
+
+getTrailsRec :: Line -> Int -> [Line] -> [Trail]
+getTrailsRec x c [] = [Trail x c]
+getTrailsRec x c (y : ys) | isBlankLine y = getTrailsRec x (c + 1) ys
+getTrailsRec x c (y : ys) = Trail x c : getTrailsRec y 0 ys
